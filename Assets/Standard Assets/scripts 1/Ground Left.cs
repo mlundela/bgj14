ff8d7488840908647a42ea2487f88d154b9f77d4
kor8der <orokkum@gmail.com>
2014-01-25 17:35:25 +0100

    Adds assets, including prefabs

---
+++ Assets/Standard Assets/scripts 1/Ground Left.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Left : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		//transform.localPosition.x = transform.localPosition.x-3;
+		rigidbody2D.velocity = new Vector2 (-3.0f ,  0.0f);
+
+	}
+}
