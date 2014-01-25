ffaeac449966337329c1f37352acc7701b371c43
Mads André Lundeland <lundeland@gmail.com>
2014-01-25 15:45:58 +0100

    Scripts for health and power ups

---
+++ Assets/Scripts/HealthScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthScript : MonoBehaviour {
+
+
+	int health = 1000;
+
+	void Update () {
+		health -= 1;
+	}
+
+	void OnGUI() {
+		GUI.Box(new Rect(10, 10, 100, 90), "Healh: " + health);
+	}
+
+	public void IncreaseHealth(int amount) {
+		health = Mathf.Min(health + amount, 1000);
+	}
+
+}
