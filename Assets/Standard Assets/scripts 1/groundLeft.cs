cb37966d71e635180347162fd23182856acd1de1
Mads André Lundeland <lundeland@gmail.com>
2014-01-25 20:10:36 +0100

    spawn test

--- Assets/Standard Assets/scripts 1/groundLeft.cs
+++ Assets/Standard Assets/scripts 1/groundLeft.cs
@@ -3,15 +3,7 @@ using System.Collections;
 
 public class groundLeft : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
 	void Update () {
-		//if(rigidbody2D.transform.position.)
 		rigidbody2D.velocity = new Vector2 (-3.0f ,  0.0f);
-
 	}
 }
