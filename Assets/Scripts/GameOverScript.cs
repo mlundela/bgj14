62b5514782b3c836f0b8bc7728e507f2d5368eaf
Mads André Lundeland <lundeland@gmail.com>
2014-01-26 15:13:01 +0100

    new game over screen

--- Assets/Scripts/GameOverScript.cs
+++ Assets/Scripts/GameOverScript.cs
@@ -4,6 +4,6 @@ using System.Collections;
 public class GameOverScript : MonoBehaviour {
 
 	void OnGUI() {
-		GUI.Box(new Rect(0, 0, Screen.height, Screen.width), "GAME OVER\nScore: " + PlayerPrefs.GetInt("Score"));
+		GUI.Box(new Rect(Screen.width/2 - 50, Screen.height/2 + 200, 100, 30), "Score: " + PlayerPrefs.GetInt("Score"));
 	}
 }
