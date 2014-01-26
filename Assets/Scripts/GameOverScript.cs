a6f6106c8c661aac786927e0d4f7212466dc99ec
Mads André Lundeland <lundeland@gmail.com>
2014-01-26 15:40:45 +0100

    game over screen updated

--- Assets/Scripts/GameOverScript.cs
+++ Assets/Scripts/GameOverScript.cs
@@ -4,9 +4,9 @@ using System.Collections;
 public class GameOverScript : MonoBehaviour {
 
 	void OnGUI() {
-		GUI.Box(new Rect(Screen.width/2 - 50, Screen.height/2 + 200, 100, 30), "Score: " + PlayerPrefs.GetInt("Score"));
+		GUI.Box(new Rect(0, 0, 100, 25), "Score: " + PlayerPrefs.GetInt("Score"));
 
-		if (GUI.Button (new Rect (Screen.width / 2 - 30, Screen.height / 2 + 240, 60, 30), "Retry?")) {
+		if (GUI.Button (new Rect (Screen.width / 2 - 30, Screen.height / 2 - 10, 60, 30), "Retry?")) {
 			Application.LoadLevel(0);		
 		}
 	}
--- Assets/Scripts/HealthScript.cs
+++ Assets/Scripts/HealthScript.cs
@@ -20,7 +20,7 @@ public class HealthScript : MonoBehaviour {
 	}
 
 	void OnGUI() {
-		GUI.Box(new Rect(10, 10, 100, 90), "Healh: " + health);
+		//GUI.Box(new Rect(10, 10, 100, 90), "Healh: " + health);
 	}
 
 	public void IncreaseHealth(int amount) {
