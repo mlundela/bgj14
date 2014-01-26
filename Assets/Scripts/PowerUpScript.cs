1168d3723d880fc0d8eff9d5c94d2d1f61b4c245
kor8der <orokkum@gmail.com>
2014-01-26 04:14:28 +0100

    Added several fixes for the second time, last commit was accidentally disappeared. This adds to the functionality of several prefabbs, and also implements sound. Buggy sound as of now

--- Assets/Scripts/PowerUpScript.cs
+++ Assets/Scripts/PowerUpScript.cs
@@ -4,14 +4,25 @@ using System.Collections;
 public class PowerUpScript : MonoBehaviour {
 
 	HealthScript hs;
+	bool touched = false;
+	public AudioSource sound;
 
 	void OnTriggerEnter2D(Collider2D other) {
 		Debug.Log("OnTriggerEnter2D");
-		if (other.tag == "Player") {
+		if (other.tag == "Player" && touched == false) {
 			hs = other.GetComponent<HealthScript>();
 			hs.IncreaseHealth(200);
-			Destroy (this.gameObject);
+			touched = true;
+			sound.Play();
+
+
 		}
 		//Destroy (this.gameObject);
+	}
+	void FixedUpdate () {
+	if (!sound.isPlaying&&touched) {
+			Destroy (this.gameObject);
+				}
+
 	}
 }
---
+++ Assets/Scripts/volumeDelayLoop.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class volumeDelayLoop : MonoBehaviour {
+
+	public AudioSource sound;
+	
+	
+	// Use this for initialization
+	void Start () {
+	
+		sound.PlayDelayed (104.0f);
+		sound.loop = true;
+
+		//player.transform.position.x
+	}
+
+}
