b7f17a93a2fe0fd8f8c079d8a04aaaeb69253892
kor8der <orokkum@gmail.com>
2014-01-26 16:19:05 +0100

    Spawns enemies, edits physics

--- Assets/Scripts/groundLeft.cs
+++ Assets/Scripts/groundLeft.cs
@@ -1,9 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
+
 public class groundLeft : MonoBehaviour {
 
+	public float leftSpeed = -2.0f;
+
 	void Update () {
-		rigidbody2D.velocity = new Vector2 (-2.0f ,  0.0f);
+		rigidbody2D.velocity = new Vector2 (leftSpeed ,  0.0f);
 	}
 }
