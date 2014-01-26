da586acfe6bd42f14ba513c4bae1b1e7a4643c26
Mads André Lundeland <lundeland@gmail.com>
2014-01-26 13:37:57 +0100

    add stuff

---
+++ Assets/Scripts/MaterialChangerScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MaterialChangerScript : MonoBehaviour {
+
+
+	void FixedUpdate() {
+		float newAlpha = (Mathf.Sin(Time.time / 2f) + 1f) / 2f;
+		//float newAlpha = .5f;
+		Debug.Log("New alpha: " + newAlpha);
+		Color originalColour = renderer.material.color;
+		renderer.material.color = new Color(originalColour.r, originalColour.g, originalColour.b, newAlpha);
+	}
+}
---
+++ Assets/Scripts/ScrollScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollScript : MonoBehaviour {
+
+
+	public float speed = 0;
+
+	void Update () {
+		//renderer.material = renderer.materials[1];
+		renderer.material.mainTextureOffset = new Vector2 (Time.time * speed, 0f);
+	}
+}
